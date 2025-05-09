904e19e47bdc34ecacc9992de1920f6354234b48
Isabella Bernal <124463723+Ibellastar@users.noreply.github.com>
2025-05-09 13:57:40 -0500

    Update multiplier.c

--- src/multiplier.c
+++ src/multiplier.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Instructions: Create a function called multiplier that takes in a and b as parameters, return the product of a and b
 int multiply(int a, int b) {
      /* Code here */
 }
